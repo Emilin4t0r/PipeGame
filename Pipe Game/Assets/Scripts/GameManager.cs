c8e505c5ec04d7298fe71c41cc1b1f3b9f9b8ae1
Emilin4t0r <virolainen.emil@gmail.com>
2020-08-15 12:33:39 +0300

    Valve turning

---
+++ Pipe Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float waterTemp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Pipe Game/Assets/Scripts/HotValve.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotValve : MonoBehaviour {
+
+    public float turnSpeed;
+    float turnLimit;
+    bool turning;
+
+    void Start() {
+
+    }
+
+    void FixedUpdate() {
+        print(transform.eulerAngles.z);
+        if (turning) {
+            if (turnLimit == 355)
+                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + turnSpeed);
+            else if (turnLimit == 5)
+                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z - turnSpeed);
+            if (transform.eulerAngles.z >= 355 || transform.eulerAngles.z <= 5) {
+                turning = false;
+            }
+        }
+    }
+
+    private void OnMouseDown() {
+        turning = true;
+        if (transform.eulerAngles.z >= 355)
+            turnLimit = 5;
+        else if (transform.eulerAngles.z <= 5)
+            turnLimit = 355;
+    }
+}
