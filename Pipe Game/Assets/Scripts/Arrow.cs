7e64645b0cf0c895c0d5e4d9f69ae25e40c69728
Emilin4t0r <virolainen.emil@gmail.com>
2020-08-17 18:00:14 +0300

    Added placeholder for spa view and camera panning

---
+++ Pipe Game/Assets/Scripts/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    GameObject toggler;
+    public enum ArrowType { Up, Down }
+    public ArrowType arrowType;
+
+    void Start() {
+
+        toggler = transform.GetChild(0).gameObject;
+
+    }
+
+    private void OnMouseEnter() {
+        toggler.SetActive(true);
+    }
+    private void OnMouseExit() {
+        toggler.SetActive(false);
+    }
+    private void OnMouseDown() {
+        if (arrowType == ArrowType.Up) {
+            if (!CameraMover.Instance.movingDown)
+                CameraMover.Instance.GoToRoom("up");
+        }
+        else if (arrowType == ArrowType.Down) {
+            if (!CameraMover.Instance.movingUp)
+                CameraMover.Instance.GoToRoom("down");
+        }
+    }
+}
---
+++ Pipe Game/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+    public static CameraMover Instance;
+
+    public Vector3 boilerPos;
+    public Vector3 spaPos;
+
+    public bool movingUp;
+    public bool movingDown;
+
+    public float moveSpeed;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void FixedUpdate() {
+
+        if (movingUp) {
+            moveSpeed = Vector3.Distance(transform.position, spaPos) / 10;
+            if (transform.position.y < 10) {
+                transform.position = new Vector3(0, transform.position.y + moveSpeed, -10);
+            }
+            else {
+                movingUp = false;
+                transform.position = spaPos;
+            }
+        }
+
+        if (movingDown) {
+            moveSpeed = Vector3.Distance(transform.position, boilerPos) / 10;
+            if (transform.position.y > 0.08f) {
+                transform.position = new Vector3(0, transform.position.y - moveSpeed, -10);
+            }
+            else {
+                movingDown = false;
+                transform.position = boilerPos;
+            }
+        }        
+    }
+
+    public void GoToRoom(string direction) {
+
+        if (direction == "up") {
+            movingUp = true;
+        }
+        if (direction == "down") {
+            movingDown = true;
+        }
+    }
+}
